2c7ce25ed111400a40d2aecc11e9b9ef2b57810d
Teodora <cvetanovska.teodora@gmail.com>
2025-12-25 16:07:17 +0100

    Fixing flaky locator

--- pages/search_page.py
+++ pages/search_page.py
@@ -9,13 +9,17 @@ class SearchPage(BasePage):
     def search_for_streamer_channels(self, text):
         self.page.fill(self.search_input, text)
         self.page.press(self.search_input, "Enter")
-        button_view_all_results = self.page.locator(
-            'p[aria-label="View All Channel Search Results"]')
+        button_view_all_results = self.page.locator("a:has-text('Channels')")
+
         button_view_all_results.click()
         self.page.get_by_role("list").wait_for(state="visible")
 
     def click_on_any_streaming_video(self):
         streaming_videos = self.page.locator("button.tw-link")
+        popup_btn = self.page.locator(
+            '[data-a-target="content-classification-gate-overlay-start-watching-button"]')
+        if popup_btn.count() > 0 and popup_btn.is_visible():
+            popup_btn.click()
         count = streaming_videos.count()
         # Here the documentation is not that clear - click on any video
         # There are many ways to solve this
